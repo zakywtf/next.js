a3fb00788fb3ac545691e089bd3d3d774f1ebb7d
nova zaky <novazaky1@gmail.com>
2020-04-10 22:29:41 +0700

    initial git

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+// import Header from '../components/Header';
+import Layout from '../components/MyLayout';
+
+const About = props => (
+    <Layout>
+        <h1>Ini About</h1>
+    </Layout>
+)
+
+export default About
---
+++ pages/post.js
@@ -0,0 +1,10 @@
+import {useRouter} from 'next/router';
+import Layout from '../components/MyLayout';
+
+const Post = props => (
+    <Layout>
+        <h1>Ini Post</h1>
+    </Layout>
+)
+
+export default Post
